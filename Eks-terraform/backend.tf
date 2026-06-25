53dbf54a45551afae68b0519ac7fa48ff5308aa0
abdallaSalaheldin <136724372+abdallaSalaheldin@users.noreply.github.com>
2026-06-25 10:17:05 +0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "abdalla-eks-2026" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
